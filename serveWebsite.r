36487cb4410f92bd396a55a1c73cfc35dc2f7e17
Jason Caballes <jason@macbook-pro-56.mynetworksettings.com>
2024-02-11 17:04:07 -0500

    - Created a docs directory to host website - Updated serverWebsite.r to point to docs for output -updated _bookdown.yml to point to docs for output

--- serveWebsite.r
+++ serveWebsite.r
@@ -1,14 +1,10 @@
 run_test_site <- function() {
-  require(bookdown)
-
-  serve_book(dir = getwd(), output_dir = "bookOutput", preview = TRUE,
-             in_session = TRUE, quiet = FALSE)
-  noquote('http://127.0.0.1:4321')
-
+    require(bookdown)
+    
+    serve_book(dir = getwd(), output_dir = "docs", preview = TRUE,
+               in_session = TRUE, quiet = FALSE)
+    noquote('http://127.0.0.1:4321')
+    
 }
 
 run_test_site()
-
-
-
-
