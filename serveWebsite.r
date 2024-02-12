a9cfe827c436629ed622917c70935cfe2b81252e
Jason Caballes <jason@macbook-pro-56.mynetworksettings.com>
2024-02-11 22:33:10 -0500

    Updated serveWebsite.R to auto create .jekyll file in docs directory

--- serveWebsite.r
+++ serveWebsite.r
@@ -1,10 +1,13 @@
-run_test_site <- function() {
+serve_site <- function() {
     require(bookdown)
     
+    # Creates book to run locally
     serve_book(dir = getwd(), output_dir = "docs", preview = TRUE,
                in_session = TRUE, quiet = FALSE)
     noquote('http://127.0.0.1:4321')
     
+    # writes an empty .nojekll file to host on github pages
+    file.create(paste(getwd(), "/docs", "/.nojekyll", sep=''))
 }
 
-run_test_site()
+serve_site()
